960586fb664930556f1f9168c4426b929be67682
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2021-12-10 19:13:36 -0500

    Center hero image, menu and nav in home page

---
+++ src/Components/HeroImage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Style/HeroImage.css';
+import { Link } from 'react-router-dom';
+
+function HeroImage() {
+    // console.log(teams)
+
+    return(
+        <div className="hero__img">
+            
+        </div>
+    )
+  
+} 
+
+export default HeroImage;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from 'react';
 import { useGlobalContext } from '../context'; 
 import Teams from '../Components/Teams';
 import PageMenu from '../Components/PageMenu';
+import HeroImage from '../Components/HeroImage';
+import '../Style/Home.css';
 
 function Home() {
   const {shortTeams} = useGlobalContext()
 
   return<main>
       <PageMenu/>
+      <HeroImage />
     </main>
   
 } 
