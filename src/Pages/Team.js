e27c4c969719362444cbe955ecd9a5f8845552ea
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2021-12-18 20:20:28 -0500

    Filter for jerseys created

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -1,24 +1,28 @@
-import React,{ useEffect} from 'react';
+import React,{ useState} from 'react';
 import { Link, useParams} from "react-router-dom";
 import data from '../data.json'
 import '../Style/App.css'; 
 import '../Style/Team.css'
-import { useGlobalContext } from '../context';
 
 function Team() {
   const { team } = useParams()
-  const {setOpenPageMenu} = useGlobalContext()
-
-  useEffect(() =>{
-    setOpenPageMenu(false)
-  },[])
-
-  console.log('team page')
+  const [filterJerseys, setFilterJerseys] = useState(false)
 
   return(
-    <div>
-      <div className="jerseys__container">
-      {data.map((item) =>{
+    <div className='team__page--cont'>
+      <div className="filter">
+        <input 
+          type="checkbox"  
+          id="checkbox" 
+          onChange={() => setFilterJerseys(!filterJerseys)}
+        />
+        <label htmlFor="checkbox" className={filterJerseys ? "label active" : "label"}>
+          <div className="filtered"></div>
+        </label>
+        <small>{filterJerseys ? "Show All Jerseys" : "Show Only City Edition"}</small>
+      </div>
+      <div className={filterJerseys ? "jerseys__container hide" : "jerseys__container"}>
+        {data.map((item) =>{
           if(item.short === team){
             const {
               id,
@@ -29,10 +33,33 @@ function Team() {
               edition,
               year
             } = item
-            // const newJersey = {
-            //   id, image, index, team, short, edition, year
-            // }
-            // console.log(newJersey)
+            return<Link
+              to={`/jerseys/${id}`}
+              key={index}
+              className="jersey__link"
+            >
+              <div className="jersey__link--info">
+                <img src={image} />
+                <div className="jerseys__info">
+                  <h3>{team} {year} {edition} jersey</h3>
+                </div>
+              </div>
+            </Link>
+          }
+        })}
+      </div>
+      <div className={filterJerseys ? "jerseys__container" : "jerseys__container hide"}>
+        {data.map((item) =>{
+          if(item.short === team && item.edition === "city"){
+            const {
+              id,
+              image,
+              index,
+              team,
+              short,
+              edition,
+              year
+            } = item
             return<Link
               to={`/jerseys/${id}`}
               key={index}
